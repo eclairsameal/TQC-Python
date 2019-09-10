6b062ba4b18d1d29e844a453cfd7ce1efaa1e7ae
Fenrir <fenrireclair@gmail.com>
2019-09-10 11:26:52 +0900

    解703 (ok)

--- 第7類：數組（Tuple）、集合（Set）以及詞典（Dictionary）/PYD703.py
+++ 第7類：數組（Tuple）、集合（Set）以及詞典（Dictionary）/PYD703.py
@@ -1,1 +1,13 @@
 # TODO
+list = []
+while True :
+    x = input()
+    if(x == 'end'):
+        break
+    list.append(x)
+tup = tuple(list)
+print(tup)
+pr_tup = list[:3]
+print(tuple(pr_tup))
+pr_tup = list[-3:]
+print(tuple(pr_tup))
