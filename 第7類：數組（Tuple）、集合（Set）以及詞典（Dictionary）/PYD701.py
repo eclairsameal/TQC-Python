63db582c9b0116961a32548fccee2471ee095e15
Fenrir <fenrireclair@gmail.com>
2019-09-10 09:03:30 +0900

    701 解題(OK)

--- 第7類：數組（Tuple）、集合（Set）以及詞典（Dictionary）/PYD701.py
+++ 第7類：數組（Tuple）、集合（Set）以及詞典（Dictionary）/PYD701.py
@@ -1,6 +1,16 @@
 # TODO
-
-
+list =[]
+while True:
+    x = int(input())
+    if(x==-9999):
+        break
+    list.append(x)  # リストに要素を追加する
+tuple_list = tuple(list) #リスト(list)をタプル(tuple)に変換する
+print(tuple_list)
+print('Length:', len(tuple_list))
+print('Max:', max(tuple_list))
+print('Min:', min(tuple_list))
+print('Sum:', sum(tuple_list))
 
 """
 Length: _
