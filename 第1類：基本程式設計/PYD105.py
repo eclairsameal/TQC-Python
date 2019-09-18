3f0035e1d6c907a68309c6ad9cf4b484a669ad54
Fenrir <fenrireclair@gmail.com>
2019-09-18 10:00:01 +0900

    解105(ok)

---
+++ 第1類：基本程式設計/PYD105.py
@@ -0,0 +1,6 @@
+h = eval(input())
+w = eval(input())
+print("Height = {:.2f}".format(h))
+print("Width = {:.2f}".format(w))
+print("Perimeter = {:.2f}".format((2 * (h + w))))
+print("Area = {:.2f}".format(h * w))
