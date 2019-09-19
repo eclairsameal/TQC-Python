d5a917cb1ee4e619c11c351e15db5b636017bbc2
Fenrir <fenrireclair@gmail.com>
2019-09-19 16:44:02 +0900

    解204(ok)

---
+++ 第2類：選擇敘述/PYD204.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+op = input()
+
+if op=='+':
+    print(a + b)
+elif op=='-':
+    print(a - b)
+elif op=='*':
+    print(a * b)
+elif op=='/':
+    print(a / b)
+elif op=='//':
+    print(a // b)
+elif op=='%':
+    print(a % b)
