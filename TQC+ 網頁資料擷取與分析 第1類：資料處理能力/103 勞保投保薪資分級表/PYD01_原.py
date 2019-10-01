8e7d5fc252ae1cc5c4971c5ed44079d539acd884
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2019-10-01 11:14:05 +0900

    Add files via upload

---
+++ TQC+ 網頁資料擷取與分析 第1類：資料處理能力/103 勞保投保薪資分級表/PYD01_原.py
@@ -0,0 +1,11 @@
+# 載入 yaml 與 json 模組
+import ___
+import ___
+
+# 讀取 json 檔案
+with ___("___", encoding='utf-8-sig') as file:
+    data = ___.___(___)
+
+# 寫入 yaml 檔案
+with ___("___", "___", encoding="utf-8") as f:
+    ___.___(data, f, default_flow_style=False, allow_unicode=True)
