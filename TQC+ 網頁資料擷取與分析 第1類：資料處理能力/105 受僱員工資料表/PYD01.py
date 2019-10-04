ef1607c2782be9a660cdb018d7cb910f21cd49f2
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2019-10-04 11:58:48 +0900

    Update PYD01.py

--- TQC+ 網頁資料擷取與分析 第1類：資料處理能力/105 受僱員工資料表/PYD01.py
+++ TQC+ 網頁資料擷取與分析 第1類：資料處理能力/105 受僱員工資料表/PYD01.py
@@ -1,17 +1,17 @@
 # 載入 sqlite3 模組
-import ___
+import sqlite3
 
 # 建立資料庫連結
-con = ___.___('___')
+con = sqlite3.connect('read.db')
 # 建立cursor物件
-___ = con.___
+c = con.cursor()
 
 # 查詢Employee資料表
-___.___("SELECT * FROM Employee")
+cursor = c.execute("SELECT * FROM Employee")
 
 # 印出查詢結果
-for ___ in ___:
-    print(___)
+for row in cursor:
+    print(row)
 
 # 關閉與資料庫的連結
 con.close()
