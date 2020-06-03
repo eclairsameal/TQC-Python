2b9c3ab40f11cfdc807f1de1fcf15ecbdf960091
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2020-06-03 14:22:46 +0900

    Add files via upload

---
+++ 第2類：選擇敘述/PYD205.py
@@ -0,0 +1,15 @@
+# TODO
+
+n = input()
+if n.isalpha():
+    print(n,"is an alphabet.")
+elif n.isdigit():
+    print(n,"is a number.")
+else:
+    print(n,"is a symbol.")
+
+"""
+_ is an alphabet.
+_ is a number.
+_ is a symbol.
+"""
