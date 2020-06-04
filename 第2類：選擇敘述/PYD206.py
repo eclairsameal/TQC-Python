e478d3a99308fe3ec3aa74df4700994c4f9d819f
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2020-06-04 20:48:04 +0900

    Add files via upload

---
+++ 第2類：選擇敘述/PYD206.py
@@ -0,0 +1,19 @@
+x = int(input())
+if x <= 100 and x >= 80:
+    print('A')
+elif x <= 79 and x >= 70:
+    print('B')
+elif x <= 69 and x >= 60:
+    print('C')
+else:
+    print('F')
+'''    
+if 80 <= x <= 100:
+    print('A')
+elif 70 <= x <= 79:
+    print('B')
+elif 60 <= x <= 69:
+    print('C')
+else:
+    print('F')
+'''
