698ba93f6e28ddc5f3f8a9b96865e3a6c0c9898c
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2020-06-09 21:16:37 +0900

    408 ok

---
+++ 第4類：進階控制流程/PYD408.py
@@ -0,0 +1,15 @@
+even = 0
+odd = 0
+for i in range(10):
+    n = int(input())
+    if n%2==0:
+        even+=1
+    else:
+        odd+=1
+print("Even numbers: {}".format(even))
+print("Odd numbers: {}".format(odd))
+
+"""
+Even numbers: _
+Odd numbers: _
+"""
