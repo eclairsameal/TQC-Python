2962815f8c0c80691087b1dfeb943aeb315f32db
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2020-06-10 16:43:28 +0900

    501OK

---
+++ 第5類：函式(Function)/PYD501.py
@@ -0,0 +1,14 @@
+def compute(d, s, n):
+    print("Department: {}".format(d))
+    print("Student ID: {}".format(s))
+    print("Name: {}".format(n))
+depart = input()
+student_id = input()
+name = input()
+compute(depart, student_id, name)
+
+"""
+Department: _
+Student ID: _
+Name: _
+"""
