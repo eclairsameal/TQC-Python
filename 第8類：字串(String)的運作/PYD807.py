2d7214adc2044572bdd508817e642267889bf5cf
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2020-07-21 20:49:53 +0900

    807 ok

---
+++ 第8類：字串(String)的運作/PYD807.py
@@ -0,0 +1,6 @@
+str_list = input().split(' ')
+num_list = [int(str_list[i]) for i in range(len(str_list))]
+
+list_sum = sum(num_list)
+print("Total = {:d}".format(list_sum))
+print("Average = {:.1f}".format(list_sum/len(num_list)))
