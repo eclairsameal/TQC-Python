3f71d2711b223b5cdbda325e3df9088c1c0da248
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2020-07-21 19:28:05 +0900

    708 ok

--- 第7類：數組（Tuple）、集合（Set）以及詞典（Dictionary）/PYD708.py
+++ 第7類：數組（Tuple）、集合（Set）以及詞典（Dictionary）/PYD708.py
@@ -1,7 +1,18 @@
-#TODO
-
-    #Key: (後方有一空白格)
-    key = input("Key: ")
+def input_dir():
+    input_dir = {}
+    while True:
+        key = input("Key: ")
+        if key=="end":
+            break
+        input_dir[key] = input("Value: ")
     
-    #Value: (後方有一空白格)
-    value = input("Value: ")
+    return input_dir
+
+print('Create dict1:')
+dir_1 = input_dir()
+print('Create dict2:')
+dir_2 = input_dir()
+
+dir_1.update(dir_2)
+for i in sorted(dir_1.keys()):
+  print(i,": ",dir_1[i],sep="")
