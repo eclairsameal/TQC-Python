a5b677b39f17093aa2e246b78daa710fc725eee2
Koneko <61724224+Koneko-TJ@users.noreply.github.com>
2020-10-28 15:19:50 +0900

    Update PYD203.py
    Add George version

--- 第2類：選擇敘述/PYD203.py
+++ 第2類：選擇敘述/PYD203.py
@@ -3,3 +3,16 @@ if y % 4 ==0 and y % 100 != 0 or y % 400 == 0:
     print("{} is a leap year.".format(y))
 else :
     print("{} is not a leap year.".format(y))
+
+    
+# version: George
+year = int(input('Please input year:'))
+
+if( year % 400 == 0):
+    print('{} is a leap year.'.format(year))
+elif( year % 100 == 0):
+    print('{} is a normal year.'.format(year))
+elif( year % 4 == 0):
+    print('{} is a leap year.'.format(year))
+else:
+    print('{} is a normal year.'.format(year))
