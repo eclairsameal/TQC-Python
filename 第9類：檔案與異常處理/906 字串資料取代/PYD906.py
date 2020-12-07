01e865b2e78ad02a8629a19c03f4117d6d51f363
Fenrir <fenrireclair@gmail.com>
2020-12-07 19:50:28 +0900

    906 ok

---
+++ 第9類：檔案與異常處理/906 字串資料取代/PYD906.py
@@ -0,0 +1,12 @@
+f_name = input()
+str_old = input()
+str_new = input()
+fp = open(f_name, "r+")
+data = fp.read()
+
+print("=== Before the replacement")
+print(data)
+
+print("=== After the replacement")
+data = data.replace(str_old, str_new)
+print(data)
