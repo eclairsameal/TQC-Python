1b5b9cc2bbfe0352f6387c6c48ebdeca0320f79d
Fenrir <fenrireclair@gmail.com>
2020-12-07 19:45:13 +0900

    905 ok

---
+++ 第9類：檔案與異常處理/905 字串資料刪除/PYD905.py
@@ -0,0 +1,16 @@
+f_name = input()
+string = input()
+fp = open(f_name, 'r+')
+datas = fp.read()
+
+print("=== Before the deletion")
+print(datas)
+
+print("=== After the deletion")
+datas = datas.replace(string, "")
+print(datas)
+
+fp.seek(0)
+fp.write(datas)
+fp.write(datas)
+fp.close()
