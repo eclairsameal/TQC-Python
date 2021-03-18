c31742fc63161ec37076c80e879a025d0833a9c0
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-03-18 16:48:22 +0900

    新增寫法

--- 第8類：字串(String)的運作/PYD807.py
+++ 第8類：字串(String)的運作/PYD807.py
@@ -1,6 +1,13 @@
 str_list = input().split(' ')
-num_list = [int(str_list[i]) for i in range(len(str_list))]
 
-list_sum = sum(num_list)
+for i in range(len(str_list)):     # string -> int
+    str_list[i] = int(str_list[i])
+
+#str_list = [int(str_list[i]) for i in range(len(str_list))] # string -> int
+
+#str_list = list(map(int, str_list)) # string -> int
+
+#print(str_list)
+list_sum = sum(str_list)
 print("Total = {:d}".format(list_sum))
-print("Average = {:.1f}".format(list_sum/len(num_list)))
+print("Average = {:.1f}".format(list_sum/len(str_list)))
