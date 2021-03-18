232030a362086a92d86aadd528c1c0ce57909a92
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-03-18 16:55:46 +0900

    新增方法

--- 第8類：字串(String)的運作/PYD808.py
+++ 第8類：字串(String)的運作/PYD808.py
@@ -1,9 +1,25 @@
 ssn_list = input().split('-')
-
+'''
+# index
+if ssn_list[0].isdigit() and ssn_list[1].isdigit() and ssn_list[2].isdigit():
+    print('Valid SSN')
+else:
+    print('Invalid SSN')
+'''
+'''
+# flag
 flag = True
 for i in range(len(ssn_list)):
     if not ssn_list[i].isdigit():
         print('Invalid SSN')
         flag = False
 if flag == True:
+    print('Valid SSN')
+'''    
+# for else
+for i in range(len(ssn_list)):
+    if not ssn_list[i].isdigit():
+        print('Invalid SSN')
+        break
+else:
     print('Valid SSN')
