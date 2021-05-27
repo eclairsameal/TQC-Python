2d755d214cfd4058f89e2c0320a2a1a5aeb6cc34
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 22:13:32 +0900

    Update PYD208.py

--- 第2類：選擇敘述/PYD208.py
+++ 第2類：選擇敘述/PYD208.py
@@ -9,3 +9,20 @@ if num <= 9:
     print(num)
 else:
     print(hex[num-10])
+    
+# solution 3
+num = int(input())
+if num <= 9:
+    print(num)
+elif num == 10:
+    print('A')
+elif num == 11:
+    print('B')
+elif num == 12:
+    print('C')
+elif num == 13:
+    print('D')
+elif num == 14:
+    print('E')
+elif num == 15:
+    print('F')
