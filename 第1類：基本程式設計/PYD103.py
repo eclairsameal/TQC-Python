245d9873340873af774713bcd11f8e26987369f0
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 15:31:06 +0900

    Update PYD103.py

--- 第1類：基本程式設計/PYD103.py
+++ 第1類：基本程式設計/PYD103.py
@@ -5,7 +5,16 @@ for i in range(4):
 #右詰め
 print("|{:>10s} {:>10s}|".format(a[0], a[1]))
 print("|{:>10s} {:>10s}|".format(a[2], a[3]))
-
 #左詰め
 print("|{:<10s} {:<10s}|".format(a[0], a[1]))
 print("|{:<10s} {:<10s}|".format(a[2], a[3]))
+
+
+a = input()
+b = input()
+c = input()
+d = input()
+print("|{:>10s} {:>10s}|".format(a, b))
+print("|{:>10s} {:>10s}|".format(c, d))
+print("|{:<10s} {:<10s}|".format(a, b))
+print("|{:<10s} {:<10s}|".format(c, d))
