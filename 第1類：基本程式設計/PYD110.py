46f5a62e4199e77d24d4560306d9152d581b8c21
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 16:29:13 +0900

    Update PYD110.py

--- 第1類：基本程式設計/PYD110.py
+++ 第1類：基本程式設計/PYD110.py
@@ -1,7 +1,12 @@
 from math import pi, tan, pow
-
 n = eval(input())
 s = eval(input())
-
 area = (n * pow(s, 2))/(4 * tan(pi/n))
+print('Area = {:.4f}'.format(area))
+
+
+import math
+n = eval(input())
+s = eval(input())
+area = (n * math.pow(s, 2))/(4 * math.tan(math.pi/n))
 print('Area = {:.4f}'.format(area))
