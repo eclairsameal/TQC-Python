d5644ceccc8f2b990b328f4c62e394cb4c028190
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 14:34:41 +0900

    Update PYD105.py

--- 第1類：基本程式設計/PYD105.py
+++ 第1類：基本程式設計/PYD105.py
@@ -2,5 +2,15 @@ h = eval(input())
 w = eval(input())
 print("Height = {:.2f}".format(h))
 print("Width = {:.2f}".format(w))
+perimeter  = 2 * (h + w)
+print("Perimeter = {:.2f}".format(perimeter ))
+area = h * w
+print("Area = {:.2f}".format(area))
+
+
+h = eval(input())
+w = eval(input())
+print("Height = {:.2f}".format(h))
+print("Width = {:.2f}".format(w))
 print("Perimeter = {:.2f}".format((2 * (h + w))))
 print("Area = {:.2f}".format(h * w))
