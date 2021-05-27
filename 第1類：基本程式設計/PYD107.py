fb9294919635947c978789059e034bfa9318275a
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 15:49:35 +0900

    Update PYD107.py

--- 第1類：基本程式設計/PYD107.py
+++ 第1類：基本程式設計/PYD107.py
@@ -1,3 +1,13 @@
+n1 = eval(input())
+n2 = eval(input())
+n3 = eval(input())
+n4 = eval(input())
+n5 = eval(input())
+print(n1, n2, n3, n4, n5)
+n_sum = n1 + n2 + n3 + n4 +n5
+print("Sum = {:.1f}".format(n_sum))
+print("Average = {:.1f}".format(n_sum/5))
+
 num = []
 for i in range(5):
     x = eval(input())
