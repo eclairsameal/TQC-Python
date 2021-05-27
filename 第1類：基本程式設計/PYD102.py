fdc526e1fd6468ed10a5b40dd4d92ef9911e22cb
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 15:18:50 +0900

    Update PYD102.py

--- 第1類：基本程式設計/PYD102.py
+++ 第1類：基本程式設計/PYD102.py
@@ -1,12 +1,20 @@
 a = []
 for i in range(4):
     x = float(input())
-    a.append(x)
-    
+    a.append(x)    
 #右詰め
 print("|{:7.2f} {:7.2f}|".format(a[0], a[1]))
 print("|{:7.2f} {:7.2f}|".format(a[2], a[3]))
-
 #左詰め
 print("|{:<7.2f} {:<7.2f}|".format(a[0], a[1]))
 print("|{:<7.2f} {:<7.2f}|".format(a[2], a[3]))
+
+
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())   
+print("|{:7.2f} {:7.2f}|".format(a, b))
+print("|{:7.2f} {:7.2f}|".format(c, d))
+print("|{:<7.2f} {:<7.2f}|".format(a, b))
+print("|{:<7.2f} {:<7.2f}|".format(c, d))
