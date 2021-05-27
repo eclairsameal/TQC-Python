b10cae728404111836f141f9c1603911b9150c93
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 14:26:11 +0900

    Update PYD104.py

--- 第1類：基本程式設計/PYD104.py
+++ 第1類：基本程式設計/PYD104.py
@@ -1,6 +1,14 @@
 import math
-
 r=eval(input())
 print("Radius = {:.2f}".format(r))
 print("Perimeter = {:.2f}".format(2*r*math.pi))
 print("Area = {:.2f}".format(r*r*math.pi))
+
+
+import math
+r = eval(input())
+print("Radius = {:.2f}".format(r))
+perimeter = 2*r*math.pi
+print("Perimeter = {:.2f}".format(perimeter))
+area = r*r*math.pi
+print("Area = {:.2f}".format(area))
