02ca4c5d7ff9d8310a6412f98719e113072b6b3b
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-27 16:21:36 +0900

    Update PYD109.py

--- 第1類：基本程式設計/PYD109.py
+++ 第1類：基本程式設計/PYD109.py
@@ -2,3 +2,8 @@ from math import pi, tan, pow
 s = eval(input())
 area = (5 * pow(s, 2))/(4 * tan(pi/5))
 print('Area = {:.4f}'.format(area))
+
+import math
+s = eval(input())
+area = (5 * math.pow(s, 2))/(4 * math.tan(math.pi/5))
+print('Area = {:.4f}'.format(area))
