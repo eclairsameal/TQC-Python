3847e23f0eba0349d3c6a2fcaa6b0b27352ca585
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-06-03 19:36:40 +0900

    Update PYD510.py

--- 第5類：函式(Function)/PYD510.py
+++ 第5類：函式(Function)/PYD510.py
@@ -4,7 +4,17 @@ def Fibonacci(num):
     if(num==0 or num==1):
         return num
     else:
-        return Fibonacci(num-1) + Fibonacci(num-2)
-    
+        return Fibonacci(num-1) + Fibonacci(num-2)    
 for i in range(num):
     print(Fibonacci(i), end=" ")
+    
+
+def fib_iterative(n):
+    if n == 0:
+        return 0
+    f1, f2 = 1, 1
+    for i in range(2, n):  # 當 n < 3 時不會進入迴圈
+        f1, f2 = f2, f1 + f2
+    return f2
+for i in range(num):
+    print(fib_iterative(i), end=" ")
