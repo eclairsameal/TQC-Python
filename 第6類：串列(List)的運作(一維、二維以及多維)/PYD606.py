73ebb7dd905c59fe67240d0fa5a3381a6e72236b
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-10-21 14:24:40 +0900

    新增另一個解(二維陣列)

--- 第6類：串列(List)的運作(一維、二維以及多維)/PYD606.py
+++ 第6類：串列(List)的運作(一維、二維以及多維)/PYD606.py
@@ -8,3 +8,15 @@ for i in li:
         print("{:>4d}".format(j), end="")
     print()
     
+    
+li =[]
+for j in range(rows):
+    l = []
+    for i in range(0, cols):
+        l.append(i-j)
+    li.append(l)
+
+for j in range(rows):
+    for i in range(0, cols):
+        print("{:>4d}".format(li[j][i]), end="")
+    print()
